47d51f8a0d8741d939cb523f1297c006cf53a612
jorge betas <devops.sky@nespon.com>
2024-03-20 22:09:24 -0300

    ajustes

--- Proyecto-Final-React/front/src/pages/AsistenciaPage.js
+++ Proyecto-Final-React/front/src/pages/AsistenciaPage.js
@@ -2,7 +2,17 @@ import React from "react";
 
 const AsistenciaPage = (props) => {
     return (
-        <div>Asistencia</div>
+        <div className="container">
+            <div className="option">
+                <img src="servicio_al_cliente.jpg" alt="Servicio al Cliente" />
+                <p>Servicio al Cliente</p>
+            </div>
+            <div className="option">
+                <img src="soporte_tecnico.jpg" alt="Soporte Técnico" />
+                <p>Soporte Técnico</p>
+            </div>
+            {/* Repite este bloque para cada opción */}
+        </div>
     );
 }
 

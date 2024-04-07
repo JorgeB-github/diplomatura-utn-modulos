cd922e16afb19e1c1367a14eacf0834841295db0
jorge betas <devops.sky@nespon.com>
2024-04-06 21:10:03 -0300

    Integrador

---
+++ Proyecto-Final-Integrador/back/routes/admin/login.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var usuariosModels = require ('./../../models/usuariosModel');
+
+
+router.get('/', function(req, res, next) {
+  res.render('admin/login',{
+    layout: 'admin/layout'
+  });
+});
+
+router.post('/', async (req,res,next) => {
+  try{
+    var usuarios = req.body.usuarios; // captura la info
+    var password = req.body.password; // captura la password
+
+    var data = await usuariosModels.getUserByUsernameAndPassword(usuarios, password); // capturo los datos
+
+    if (data != undefined) {
+      res.redirect('/admin/novedades');      
+    } else {
+      res.render('admin/login', {
+        layout: 'admin/layout',
+        error: true
+      });
+    }
+  } catch (error){
+    console.log(error);
+  }
+});
+
+
+module.exports = router;
+
